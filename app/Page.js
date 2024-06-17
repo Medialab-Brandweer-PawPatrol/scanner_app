f85e515e8bd1f23bf22c89128651ef27b1c8f5d2
vrostar <mistermustache33@gmail.com>
2024-06-17 16:12:05 +0200

    added camera

--- app/Page.js
+++ app/Page.js
@@ -1,6 +1,8 @@
 import * as React from "react";
-import {StyleSheet, View} from 'react-native';
+import {StyleSheet, View, TouchableOpacity, ScrollView} from 'react-native';
 import { Button, Text, Snackbar, Portal } from 'react-native-paper';
+import { CameraView, useCameraPermissions } from 'expo-camera';
+import { useState } from 'react';
 
 const Page = () => {
   const [showMore, setShowMore] = React.useState(false);
@@ -16,7 +18,30 @@ const Page = () => {
 	console.log("hi");
   }
 
+  const [facing, setFacing] = useState('back');
+  const [permission, requestPermission] = useCameraPermissions();
+
+  if (!permission) {
+    // Camera permissions are still loading.
+    return <View />;
+  }
+
+  if (!permission.granted) {
+    // Camera permissions are not granted yet.
+    return (
+      <View style={styles.container}>
+        <Text style={{ textAlign: 'center' }}>We need your permission to show the camera</Text>
+        <Button style={styles.infoButton} onPress={requestPermission} title="grant permission" />
+      </View>
+    );
+  }
+
+  function toggleCameraFacing() {
+    setFacing(current => (current === 'back' ? 'front' : 'back'));
+  }
+
   return (
+    <ScrollView contentContainerStyle={styles.scrollContainer}>
     <View style={styles.container}>
       <Text style={styles.heading}>Real time feedback scan</Text>
       <Text style={styles.body}>Gebruik de app om de veiligheid van je huis te waarborgen. Scan de ruimtes van het huis en krijg meteen te zien waar potentieel brandgevaar is!</Text>
@@ -29,6 +54,13 @@ const Page = () => {
         <Text style={styles.body}>Om het huis optimaal te beschermen, kan de app je vragen om de gescande gevaren op te lossen. Met enkele aanbevelingen helpen we je daar gemakkelijk doorheen!</Text>
       </View>
       }
+      <CameraView style={styles.camera} facing={facing}>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity style={styles.button} onPress={toggleCameraFacing}>
+            <Text style={styles.text}>FLIP</Text>
+          </TouchableOpacity>
+        </View>
+      </CameraView>
 	  <Button style={styles.infoButton} onPress={doNotify}>Wat?</Button>
 		<Portal>
 	  <Snackbar visible={notify} onDismiss={() => {setNotify(false)}} action={{
@@ -39,10 +71,14 @@ const Page = () => {
         }}>2 Dangers detected</Snackbar>
 </Portal>
     </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
+  scrollContainer: {
+    flexGrow: 1,
+  },
   container: {
     height: '100%',
     marginTop: 32,
@@ -59,6 +95,11 @@ const styles = StyleSheet.create({
     fontSize: 14,
     textAlign: "left",
     marginBottom: 16
+  },
+  camera: {
+    flex: 1,
+    width: 380,
+    height: 480
   },
   infoButton: {
     marginBottom: 16
